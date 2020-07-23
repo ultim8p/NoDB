36296fdfbc2ca08db4e18001001230b280249eb1
itndehuip <itandehui_pacheco@hotmail.com>
2020-07-22 20:09:52 -0500

    Added methods for search with greaterThan and lowerThan operators

--- Sources/NoDB/BinarySearch/BinarySearch+Index.swift
+++ Sources/NoDB/BinarySearch/BinarySearch+Index.swift
@@ -10,8 +10,8 @@ import BinarySearch
 
 public extension Array where Element == [String: Any] {
     func binarySearch(withIndex index: Any, key: String, value: Any) -> (obj: Element, index: Int)? {
-        guard let searchResult = binarySearchAll(key: key, value: value) else { return nil }
-        for i in searchResult.lowerIndex...searchResult.upperIndex {
+        guard let searchResult = binarySearchAll(key: key, value: value), let lowerIndex = searchResult.lowerIndex, let upperIndex = searchResult.upperIndex else { return nil }
+        for i in lowerIndex...upperIndex {
             let element = self[i]
             let comparisonResult = element.compare(to: index, key: NoDBConstant.index.rawValue)
             if comparisonResult == .equal {
